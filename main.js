43c2a07b39ead898b5f5ff8b038d39dea86f43e4
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-09-04 19:25:49 +0200

    feat: add data binding, conditional and list rendering examples

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        altText: 'A pair of shocks',
+        link: 'https://www.google.es',
+        inventory: 100,
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: ["xs", "s", "m", "l", "xl"]
     }
 }) 
